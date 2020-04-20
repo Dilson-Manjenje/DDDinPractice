7664de2f2d8bb7b127f85b4898b240259843290f
dilson.manjenje <dilson.manjenje@outlook.com>
2020-04-20 21:38:10 +0100

    Add project files.

---
+++ DddInPractice.Biz/SnackMachine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DddInPractice.Biz
+{
+    public sealed class SnackMachine
+    {
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuarterCentCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarCentCount { get; set; }
+        public int TwentyDollarCentCount { get; set; }
+
+        // Inserir Dinheiro
+        // Comprar Lanche
+        // Devolver Dinheiro - Dar Troco
+
+    }
+}
